31187d6af540c56aa7d60c1477134b58247d8a1a
Федяй Виталий <vfedyay@hold.bayadera.ua>
2019-02-18 12:28:19 +0200

    fixed routes, component
    created new folder component

--- homework7/project/src/app/app-routing.module.ts
+++ homework7/project/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
+import { ListComponent } from './components/list/list.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'dashboard', component: DashboardComponent},
+{ path: 'table', component: TableComponent},
+{ path: 'list', component: ListComponent },
+{ path: 'slider', component: SliderComponent },
+{ path: 'home', component: HomeComponent },
+{ path: '**', redirectTo: '' }];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(routes);
--- homework7/project/src/app/app.module.ts
+++ homework7/project/src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TableComponent } from './table/table.component';
-import { ListComponent } from './list/list.component';
-import { SliderComponent } from './slider/slider.component';
-import {Routes, RouterModule} from '@angular/router';
-import { RadardbComponent } from './content/radardb/radardb.component';
-import { PolardbComponent } from './content/polardb/polardb.component';
-import { HomeComponent } from './home/home.component';
-
-const appRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'table', component: TableComponent},
-  { path: 'list', component: ListComponent },
-  { path: 'slider', component: SliderComponent },
-  { path: 'home', component: HomeComponent }
-];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
+import { ListComponent } from './components/list/list.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { RadardbComponent } from './components/content/radardb/radardb.component';
+import { PolardbComponent } from './components/content/polardb/polardb.component';
+import { HomeComponent } from './components/home/home.component';
+import { routing } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -32,12 +23,10 @@ const appRoutes: Routes = [
     HomeComponent
   ],
   imports: [
+    routing,
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
     MDBBootstrapModule.forRoot()
   ],
-  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
